eed343f16582649fd315649d3c04dfbe654070d1
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-28 19:38:18 +0200

    Solution

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get["POSTGRES_HOST"],
-        "NAME": os.environ.get["POSTGRES_DB"],
-        "USER": os.environ.get["POSTGRES_USER"],
-        "PASSWORD": os.environ.get["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
 
