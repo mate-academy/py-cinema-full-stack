20faee916aa9136e1446f644ae65a8bf1fd2889d
Paul Starodub <amorallex@gmail.com>
2023-04-01 15:57:59 +0400

    Updated user/models with typehint

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Any, Optional
+
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
@@ -11,7 +15,12 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(
+            self,
+            email: str,
+            password: str,
+            **extra_fields: dict[str, Any]
+    ) -> ValueError | User:
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError("The given email must be set")
@@ -21,13 +30,23 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(
+            self,
+            email: str,
+            password: Optional[str] = None,
+            **extra_fields: dict[str, Any]
+    ) -> User:
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(
+            self,
+            email: str,
+            password: str,
+            **extra_fields: dict[str, Any]
+    ) -> ValueError | User:
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
