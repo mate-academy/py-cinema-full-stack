a79957cc1521523a5c8fc91e80bc489a2a6fbde3
Paul Starodub <amorallex@gmail.com>
2023-04-01 16:04:28 +0400

    Updated user/views with typehint and permissions

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,18 +1,18 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from user.models import User
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self):
+    def get_object(self) -> User:
         return self.request.user
