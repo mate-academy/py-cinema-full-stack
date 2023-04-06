7769c3d44ffbd04d77f897e0cd8a1150040c10de
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-06 16:08:14 +0200

    Added django secret key in .env

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -24,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
-)
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
