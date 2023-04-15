19d8f54d22fd449ab0f133fba3a0969e3fc392e8
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-16 01:32:24 +0300

    Updated .env, .env.sample, settings.py

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -180,5 +178,4 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
-
 ]
