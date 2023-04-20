4355bc5536f11d2db4635bfc74c4f66129756c18
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-20 21:04:05 +0300

    Solution

--- backend/cinema/serializers.py
+++ backend/cinema/serializers.py
@@ -2,14 +2,14 @@ from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from cinema.models import (
-    Genre,
-    Actor,
-    CinemaHall,
-    Movie,
-    MovieSession,
-    Ticket,
-    Order,
+from models import (
+     Genre,
+     Actor,
+     CinemaHall,
+     Movie,
+     MovieSession,
+     Ticket,
+     Order,
 )
 
 
