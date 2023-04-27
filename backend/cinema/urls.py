e7d677e9a79042203d2cbaa85fac554e15fa4c1c
Denys Tereshchuk <terrya@ukr.net>
2023-04-27 13:26:54 +0400

    Updated cinema urls

--- backend/cinema/urls.py
+++ backend/cinema/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework import routers
 
 from cinema.views import (
@@ -18,6 +17,6 @@ router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
 
 app_name = "cinema"
