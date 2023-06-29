3f3073db6f5dc9030330d4e9fbd4ffc35a3177a0
Anastasiia <nastyashch1601@gmail.com>
2023-06-29 20:15:52 +0100

    Solution

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",]
 
 INTERNAL_IPS = [
     "127.0.0.1",
