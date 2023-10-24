eb6644e10ec19ea6a24830a4a20cdee0776228f3
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-24 19:31:15 +0200

    connection backend to frontend implementation

--- backend/cinema/urls.py
+++ backend/cinema/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 from cinema.views import (
     GenreViewSet,
@@ -21,3 +23,8 @@ router.register("orders", OrderViewSet)
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "cinema"
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
