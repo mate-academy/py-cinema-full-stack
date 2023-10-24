d2808ef7439bce7977f5628bedf6f4e5dd2f4599
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-24 18:58:03 +0300

    connected front to back

--- backend/cinema/management/commands/wait_for_db.py
+++ backend/cinema/management/commands/wait_for_db.py
@@ -1,20 +1,26 @@
+import os
 import time
-from django.db import connections
-from django.db.utils import OperationalError
-from django.core.management import BaseCommand
+import psycopg2
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to pause execution until db is available"""
-
     def handle(self, *args, **options):
-        self.stdout.write("Waiting for database...")
-        db_conn = None
-        while not db_conn:
+        self.stdout.write("Waiting for the database...")
+
+        db_ready = False
+        while not db_ready:
             try:
-                db_conn = connections["default"]
-            except OperationalError:
+                connection = psycopg2.connect(
+                    dbname=os.environ["POSTGRES_DB"],
+                    user=os.environ["POSTGRES_USER"],
+                    password=os.environ["POSTGRES_PASSWORD"],
+                    host=os.environ["POSTGRES_HOST"],
+                )
+                connection.close()
+                db_ready = True
+            except psycopg2.OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+        self.stdout.write(self.style.SUCCESS("Database is available!"))
