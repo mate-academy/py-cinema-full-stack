839c8c36fca7fa9fc86441b5467b5eedbf5740c6
Denis Dymerlii <dum.denya15@gmail.com>
2024-01-22 16:11:41 +0200

    Fixed solution

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    ''"corsheaders",
+    "" "corsheaders",
     "drf_spectacular",
     "debug_toolbar",
     "cinema",
