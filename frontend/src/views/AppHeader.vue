74679b93a86de0ccc3a92ea5d3a974765f9660f0
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-12 11:31:13 +0300

    fixed urls in frontend

--- frontend/src/views/AppHeader.vue
+++ frontend/src/views/AppHeader.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <a class="app-title" href="/">Cinema Shop</a>
     <div class="menu">
-      <a href='#/movie-sessions' :class="activeTab === 'movie-sessions' && 'active'">Movie Sessions</a>
+      <a href='${import.meta.env.VITE_API_URL}/movie-sessions' :class="activeTab === 'movie-sessions' && 'active'">Movie Sessions</a>
       <a href='#/cinema-halls' v-if="user.is_staff" :class="activeTab === 'cinema-halls' && 'active'">Cinema Halls</a>
       <a href='#/movies' :class="activeTab.match(/(movies|^$)/) && 'active'">Movies</a>
       <a href='#/genres' v-if="user.is_staff" :class="activeTab === 'genres' && 'active'">Genres</a>
