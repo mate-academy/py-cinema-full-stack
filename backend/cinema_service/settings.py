a75a50dd36eafbf0ad9e655554cf83c2bb8392ca
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-12 10:09:13 +0300

    added a number port for PostgreSQL in settings.py

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "PORT": 5432,
     }
 }
 
