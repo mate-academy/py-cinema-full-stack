9e7e0ffa2528edad518bc89e546f12e3af20d563
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-16 02:31:36 +0300

    Change ACESS_TOKEN_LIFETIME back to minutes value

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -173,7 +173,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=60 * 60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60 * 60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
