be2a56150264a9f5ea7b22d75d51af90c3e76416
ira <irynaira@gmail.com>
2025-01-22 22:55:12 +0200

    for teacher 2 22.01.25

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '172.18.0.2']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "172.18.0.2"]
 
 
 INTERNAL_IPS = [
