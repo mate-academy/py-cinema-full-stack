248213df9df9c7bf53e23ea035e3b47b49df6f98
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2025-03-26 15:27:21 +0200

    add django-cors-headers configuration to settings

--- backend/cinema/views.py
+++ backend/cinema/views.py
@@ -6,7 +6,7 @@ from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
