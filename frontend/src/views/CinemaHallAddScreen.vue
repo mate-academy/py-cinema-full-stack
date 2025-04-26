434d545c2f7370ded8f4fcfac0aafe533f2be7d7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-26 19:40:43 +0300

    CinemaHallAddScreen.vue

--- frontend/src/views/CinemaHallAddScreen.vue
+++ frontend/src/views/CinemaHallAddScreen.vue
@@ -1,21 +1,24 @@
 <template>
-  <div v-if="active && isStaff" class="cinema-halls">
-    <div class="label">Cinema Halls</div>
-    <div class="hall-container">
-      <div v-for="hall in halls" class="hall" :key="hall.id">
-        <div class="name">{{hall.name}}</div>
-        <div class="size">Size: {{hall.rows}} x {{hall.seats_in_row}}</div>
-        <div>Capacity: {{hall.capacity}}</div>
+    <div v-if="active && isStaff" class="hall-container">
+    <div class="header">Add a cinema hall</div>
+    <div class="note">Please fill in the fields in details</div>
+    <div class="container">
+      <input-item label="Name" v-model="name" width="wide"></input-item>
+      <div class="info-container">
+        <input-item label="Number of rows" v-model="countRows" width="narrow"></input-item>
+        <input-item label="Number of seats in row" v-model="countSeatsInRow" width="narrow"></input-item>
       </div>
     </div>
-    <add-btn @click="handleHallCreate"></add-btn>
+    <action-button label="Submit" @click="addCinemaHall" :disabled="!name || !countRows || !countSeatsInRow"></action-button>
   </div>
 </template>
 
 <script>
-import AddBtn from '../comps/AddBtn.vue';
-
 import axios from 'axios';
+
+import ActionButton from '../comps/ActionButton.vue';
+import InputItem from '../comps/InputItem.vue';
+
 export default {
   props: {
     isStaff: {
@@ -25,7 +28,9 @@ export default {
   },
   data: () => ({
     active: false,
-    halls: []
+    name: '',
+    countRows: 1,
+    countSeatsInRow: 1
   }),
   computed: {
     token () {
@@ -33,31 +38,35 @@ export default {
     }
   },
   methods: {
-    async fetchHalls () {
-      try {
-        const { data: halls } = await axios.get(`${import.meta.env.VITE_API_URL}/api/cinema/cinema_halls/`, {
-          headers: { Authorization: `Bearer ${this.token}` }
-        });
-        this.halls = halls;
-      } catch (err) {
-        console.error(err.response.data);
-      }
-    },
-
-    handleHallCreate () {
-      location.hash = '#/cinema-halls?add=true';
-    },
-
     hashHandler () {
-      this.active = Boolean(location.hash.match('cinema-halls$'));
-    }
-  },
-  watch: {
-    active () {
-      if (this.active) {
-        this.fetchHalls();
+      this.active = Boolean(location.hash.match('cinema-halls\\?add=true'));
+    },
+
+    async addCinemaHall () {
+      try {
+        const config = {
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+            'Content-Type': 'application/json'
+          }
+        };
+
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/api/cinema/cinema_halls`,
+          {
+            name: this.name,
+            rows: Number(this.countRows),
+            seats_in_row: Number(this.countSeatsInRow)
+          },
+          config
+        );
+
+        location.hash = '#/cinema-halls';
+      } catch (err) {
+        console.error(err);
       }
     }
+
   },
   mounted () {
     window.addEventListener('hashchange', this.hashHandler);
@@ -67,52 +76,45 @@ export default {
     window.removeEventListener('hashchange', this.hashHandler);
   },
   components: {
-    AddBtn
+    ActionButton,
+    InputItem
   }
 };
 </script>
 
 <style scoped>
-.cinema-halls {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.label {
-  font-weight: 600;
-  font-size: 50px;
-  line-height: 60px;
-  margin-bottom: 60px;
-}
-
 .hall-container {
-  display: flex;
-  gap: 65px;
-  flex-wrap: wrap;
-}
-
-.name {
-  font-weight: 700;
-  font-size: 25px;
-  line-height: 30px;
-  margin-bottom: 24px;
-}
-
-.size {
-  margin-bottom: 12px;
-}
-
-.hall {
-  width: 196px;
-  height: 190px;
-  background-color: var(--secondary-bg);
-  border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: 18px;
-  line-height: 22px;
+  gap: 24px;
+}
+
+.header {
+  font-weight: 600;
+  font-size: 50px;
+  line-height: 61px;
+}
+
+.note {
+  font-size: 25px;
+  line-height: 31px;
+}
+
+.container {
+  width: 100%;
+  max-width: 570px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  margin-bottom: 36px;
+}
+
+.info-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 60px;
 }
 </style>
