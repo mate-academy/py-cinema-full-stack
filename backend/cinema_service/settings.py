bf4443abeba1c649477bacc42ec11391b1349600
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-09 14:49:44 +0300

    Solution

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -139,7 +139,7 @@ USE_TZ = False
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/vol/web/media"
+MEDIA_ROOT = os.path.join(BASE_DIR, os.environ.get("MEDIA_ROOT", "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
