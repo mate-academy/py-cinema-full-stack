a5dca30cb803433148306142e028668cee6fa190
AndyStar <andy_star@ukr.net>
2025-05-26 13:08:22 +0300

    1) Slashes added, 2) comments removed, 3) blank lines added, 4) One too long string splitted

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -180,11 +180,8 @@ SIMPLE_JWT = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://example.com",
-    # "http://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
-    # "http://127.0.0.1:9000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
