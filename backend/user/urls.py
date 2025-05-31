75294b30bfecf281ef430ac1a4ec5708dbbe8f45
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-05-31 16:13:42 +0200

    Solution

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -12,7 +12,7 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
