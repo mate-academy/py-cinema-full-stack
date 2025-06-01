5125a3a02ac322bbadd810554f97f45a41195b65
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-01 15:53:02 +0300

    Solution

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Добро пожаловать в кинотеатр</h1>
+    <vc-calendar />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 2rem;
+}
+</style>
