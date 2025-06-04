34ffe69964bc9bb2f9cc9f5ff07157131543f4b8
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-04 14:12:28 +0200

    fix: add unaded files

--- frontend/README.md
+++ frontend/README.md
@@ -1,5 +1,3 @@
-# cinema-shop-vue-ui
-
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 import vue2 from '@vitejs/plugin-vue2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
+    vue2()
   ],
+  build: {
+    target: 'esnext'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
