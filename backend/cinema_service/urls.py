f9c91d0435cdaf6c9474a373bc83f80d98c4b68e
Yaroslav Didkivskiy <yaroslavdidkivskiy@gmail.com>
2025-07-14 13:42:59 +0300

    solution

--- backend/cinema_service/urls.py
+++ backend/cinema_service/urls.py
@@ -8,7 +8,7 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
-urlpatterns = [
+urlpatterns = ([
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("api/user/", include("user.urls", namespace="user")),
@@ -24,4 +24,5 @@ urlpatterns = [
         name="redoc",
     ),
     path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +
+               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
