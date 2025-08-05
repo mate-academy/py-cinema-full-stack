28d2ac373fb1590af6829d16c9eb1cc50962b347
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-08-06 00:49:35 +0200

    solution

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
@@ -7,6 +7,7 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
