9cc9d0a462ae848d108ee134ec93f93a0f216735
Vadym <kirilkovadim193@gmail.com>
2025-09-26 10:59:04 +0300

    Refactor frontend and settings

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -64,9 +64,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5176",
-    "http://127.0.0.1:5176",
+    "http://localhost:5173",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "cinema_service.urls"
 
