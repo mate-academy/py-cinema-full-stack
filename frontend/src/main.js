9911951af48ebdf50815b8513c30b7008b45989b
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-31 21:19:26 +0200

    Solution

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,22 @@ import './assets/main.css';
 Vue.use(VueAxios, axios);
 Vue.use(VCalendar);
 
+
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('access');
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
   render: (h) => h(App)
 }).$mount('#app');
