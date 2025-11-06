6536bc61d5d53681b773cac20af57ff81eb80ed9
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-11-06 11:49:55 +0200

    - Added missing trailing slashes to API endpoint URLs in frontend components. - Updated backend settings to use environment variables for sensitive configurations. - Modified backend dependencies and `.env` handling, including dotenv integration. - Added CORS support in backend and updated middleware configuration. - Adjusted Dockerfile for streamlined builds; removed maintainer label. - Enhanced `user` model validation for better clarity. - Updated `package-lock.json` with peer dependencies and minor adjustments. - Corrected typo in frontend README file. - Expanded requirements in `requirements.txt` for added functionalities and compatibility.

--- backend/user/models.py
+++ backend/user/models.py
@@ -32,9 +32,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
+        if not extra_fields.get("is_staff"):
             raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
+        if not extra_fields.get("is_superuser"):
             raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(email, password, **extra_fields)
