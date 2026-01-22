7478f5e6cf8b4ef11c6373bda2bed7fd4d717c29
Mykola Totsevych <totsevich02@gmail.com>
2026-01-22 16:35:32 +0100

    add solution

--- backend/cinema/management/commands/wait_for_db.py
+++ backend/cinema/management/commands/wait_for_db.py
@@ -1,20 +1,21 @@
 import time
 from django.db import connections
 from django.db.utils import OperationalError
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to pause execution until db is available"""
+    """Django command to wait for the database to be available."""
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options) -> None:
         self.stdout.write("Waiting for database...")
-        db_conn = None
-        while not db_conn:
+        db_ready = False
+        while not db_ready:
             try:
-                db_conn = connections["default"]
+                connection = connections["default"]
+                connection.cursor()
+                db_ready = True
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS("Database available!"))
