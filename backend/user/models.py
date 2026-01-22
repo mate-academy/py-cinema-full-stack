cd94206a3cfa119f961a2d8a6105b8564cee6e9d
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-22 23:54:37 +0200

    Fix of endpoints => fixed render of header routers and access

--- backend/user/models.py
+++ backend/user/models.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
