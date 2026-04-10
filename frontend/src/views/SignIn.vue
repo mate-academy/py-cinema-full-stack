6a5454417fc7d6f79be51236eb0a7303d7ac1bb6
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-04-10 11:28:20 +0200

    Add CORS support and update Vue components for improved formatting
    - Added CORS headers configuration in settings.py
    - Updated SignIn.vue and SignUp.vue with proper url path
    - Marked certain dependencies as peer in package-lock.json
    - Updated requirements.txt

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,65 +1,71 @@
 <template>
   <div class="sign-in" v-if="active">
     <h1>Sign in to Cinema Shop</h1>
-    <h2>Please enter your sign in details.
-    <a href="#/sign-up">Sign up</a>
-    here if you are not registered yet.</h2>
+    <h2>
+      Please enter your sign in details.
+      <a href="#/sign-up">Sign up</a>
+      here if you are not registered yet.
+    </h2>
     <input-item
       label="Login"
       pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
       placeholder="Email"
-      v-model="email"></input-item>
+      v-model="email"
+    ></input-item>
     <password-input v-model="password"></password-input>
     <action-button label="Sign in" @click="signIn"></action-button>
   </div>
 </template>
 
 <script>
-import ActionButton from '../comps/ActionButton.vue';
-import InputItem from '../comps/InputItem.vue';
-import PasswordInput from '../comps/PasswordInput.vue';
+import ActionButton from "../comps/ActionButton.vue";
+import InputItem from "../comps/InputItem.vue";
+import PasswordInput from "../comps/PasswordInput.vue";
 
 export default {
   data: () => ({
     active: false,
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   }),
   methods: {
-    hashHandler () {
-      this.active = Boolean(!location.hash.match('sign-up$'));
+    hashHandler() {
+      this.active = Boolean(!location.hash.match("sign-up$"));
     },
 
-    async signIn () {
+    async signIn() {
       try {
-        const { data } = await this.axios.post(`${import.meta.env.VITE_API_URL}/api/user/token`, {
-          email: this.email,
-          password: this.password
-        });
+        const { data } = await this.axios.post(
+          `${import.meta.env.VITE_API_URL}/api/user/token/`,
+          {
+            email: this.email,
+            password: this.password,
+          },
+        );
 
         const { access, refresh } = data;
 
-        localStorage.setItem('access', access);
-        localStorage.setItem('refresh', refresh);
+        localStorage.setItem("access", access);
+        localStorage.setItem("refresh", refresh);
 
-        this.$emit('log-in');
+        this.$emit("log-in");
       } catch (err) {
         console.error(err.response.data);
       }
-    }
+    },
   },
-  mounted () {
-    window.addEventListener('hashchange', this.hashHandler);
+  mounted() {
+    window.addEventListener("hashchange", this.hashHandler);
     this.hashHandler();
   },
-  beforeDestroy () {
-    window.removeEventListener('hashchange', this.hashHandler);
+  beforeDestroy() {
+    window.removeEventListener("hashchange", this.hashHandler);
   },
   components: {
     InputItem,
     PasswordInput,
-    ActionButton
-  }
+    ActionButton,
+  },
 };
 </script>
 
