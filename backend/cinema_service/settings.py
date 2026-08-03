f3cdd9efb8e4562c2b38032f99e09208f60624e8
JulianskyMe <juliakamynskaya@gmail.com>
2026-08-03 20:22:41 +0300

    Hiding secret key, fixing axios links

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -17,13 +17,12 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
