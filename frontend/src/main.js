792b0b01d3e48df6783430e415795e3d4c013e0a
Andriy <andrijbuluk@gmail.com>
2026-08-07 18:07:29 +0200

    set Axios base URL from environment variable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import VCalendar from 'v-calendar';
 
 import './assets/main.css';
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8080';
+
 Vue.use(VueAxios, axios);
 Vue.use(VCalendar);
 
