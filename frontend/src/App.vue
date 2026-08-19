e5cf6783ed09816a9a5b7fdd69e9c86b039fc98e
Maksym Kaplunovskyi <kaplunovskymv@gmail.com>
2026-08-19 20:35:58 +0200

    Connect frontend and backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -76,7 +76,7 @@ export default {
     async fetchUser () {
       const accessToken = localStorage.getItem('access');
       try {
-        const { data: user } = await this.axios.get(`${import.meta.env.VITE_API_URL}/api/user/me`,
+        const { data: user } = await this.axios.get(`${import.meta.env.VITE_API_URL}/api/user/me/`,
           { headers: { Authorization: `Bearer ${accessToken}` } });
 
         this.user = user;
@@ -87,14 +87,13 @@ export default {
 
     async refreshToken () {
       try {
-        const { data } = await this.axios.post(`${import.meta.env.VITE_API_URL}/api/user/token/refresh`, {
+        const { data } = await this.axios.post(`${import.meta.env.VITE_API_URL}/api/user/token/refresh/`, {
           refresh: localStorage.getItem('refresh')
         });
 
-        const { access, refresh } = data;
+        const { access } = data;
 
         localStorage.setItem('access', access);
-        localStorage.setItem('refresh', refresh);
         this.logIn();
       } catch (err) {
         console.error(err.response.data);
