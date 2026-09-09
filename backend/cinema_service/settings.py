c8a77015308dc8127722873b74a33e6564c89b01
VolodymyrSeniv <seniv.05@gmail.com>
2026-09-09 17:43:40 +0200

    fix: changed cors url in backed settings.py, removed odd requirements.txt, specified .emv ignorance in gitignore

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -68,8 +68,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 APPEND_SLASH = False
